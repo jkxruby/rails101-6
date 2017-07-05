86db5f333def18867960f6ab044c64cc0d079fd7
jkxruby <jkxruby@gmail.com>
2017-07-05 10:00:24 +0800

    use simple_format helper to decorate

--- app/controllers/account/groups_controller.rb
+++ app/controllers/account/groups_controller.rb
@@ -6,5 +6,4 @@ def index
 end
 
 
-
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,35 @@ class PostsController < ApplicationController
   def new
     @group = Group.find(params[:group_id])
     @post = Post.new
+    
   end
+
+  def edit
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+  end
+
+  def destroy
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    @post.group = @group
+    @post.destroy
+    redirect_to group_path(@group), alert: "say bye"
+  end
+
+  def update
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    @post.group = @group
+    @post.user = current_user
+
+    if @post.update(post_params)
+      redirect_to group_path(@group), notice: "update success"
+    else
+      render :edit
+    end
+  end
+
 
   def create
     @group = Group.find(params[:group_id])
@@ -12,7 +40,7 @@ class PostsController < ApplicationController
     @post = Post.new(post_params)
     @post.group = @group
     @post.user = current_user
-    
+
     if @post.save
       redirect_to group_path(@group)
     else
